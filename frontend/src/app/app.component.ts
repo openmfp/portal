eeb1ad6604d6710cd98b526de8dfb0e3f5d046d4
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-06-17 11:05:22 +0200

    feat: Integration of the newly created portal-server-lib, and the available services.

---
+++ frontend/src/app/app.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  standalone: true,
+  imports: [RouterOutlet],
+  template: '',
+})
+export class AppComponent {
+  title = 'frontend';
+}
