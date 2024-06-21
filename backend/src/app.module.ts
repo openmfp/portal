3b7b6e1f7c88dbe61273168c722e01ea28a2a6f3
Tobias Oetzel <tobias.oetzel@sap.com>
2024-06-21 16:36:28 +0200

    introduce k8s service providers

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,11 +2,12 @@ import { Module } from '@nestjs/common';
 import { PortalModule } from '@openmfp/portal-lib';
 import { join } from 'path';
 import { LocalServiceProviderService } from './serviceProviders/localServiceProviders';
+import { KubernetesServiceProviders } from './serviceProviders/kubernetesServiceProviders';
 
 @Module({
   imports: [PortalModule.create({
     frontendDistSources: join(__dirname, '../..', 'frontend/dist/frontend'),
-    serviceProviderService: LocalServiceProviderService,
+    serviceProviderService: KubernetesServiceProviders,
   })],
   controllers: [],
   providers: [],
---
+++ backend/src/serviceProviders/kubernetesServiceProviders.ts
@@ -0,0 +1,37 @@
+import { RawServiceProvider, ServiceProviderResponse, ServiceProviderService } from '@openmfp/portal-lib';
+import * as k8s from '@kubernetes/client-node';
+import { CustomObjectsApi } from '@kubernetes/client-node';
+
+
+export class KubernetesServiceProviders implements ServiceProviderService {
+
+  private k8sApi: CustomObjectsApi;
+
+  constructor() {
+    const kc = new k8s.KubeConfig();
+    kc.loadFromDefault()
+    this.k8sApi = kc.makeApiClient(k8s.CustomObjectsApi)
+  }
+
+  async getServiceProviders(token: string, entities: string[], context: Record<string, any>): Promise<ServiceProviderResponse> {
+    try {
+      const response = await this.k8sApi.listNamespacedCustomObject('core.openmfp.io', 'v1alpha1', 'openmfp-system','contentconfigurations');
+
+      if(!response.body['items']) {
+        return {
+          serviceProviders: []
+        }
+      }
+
+      const responseItems = response.body['items'] as any[]
+
+      let serviceProviders = responseItems.map(x => JSON.parse(x.status.configurationResult) as RawServiceProvider);
+      console.log(serviceProviders);
+      return {
+        serviceProviders: serviceProviders
+      }
+    } catch (error) {
+      console.error(error);
+    }
+  }
+}
