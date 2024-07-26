3d23a59fdaab082e0942aa977755eed8f1e8a98f
Tobias Oetzel <tobias.oetzel@sap.com>
2024-07-26 10:40:47 +0200

    feat: introduce the graphql gateway as context

---
+++ backend/src/portal-context-provider.ts
@@ -0,0 +1,15 @@
+import { PortalContextProvider } from '@openmfp/portal-lib';
+import { Injectable } from '@nestjs/common';
+import * as process from 'node:process';
+
+@Injectable()
+export class OpenmfpPortalProvider implements PortalContextProvider {
+  constructor() {}
+
+  getContextValues(): Promise<Record<string, any>> {
+    const context: Record<string, any> = {
+      "crdGatewayApiUrl": process.env.CRD_GATEWAY_API_URL,
+    };
+    return Promise.resolve(context);
+  }
+}
