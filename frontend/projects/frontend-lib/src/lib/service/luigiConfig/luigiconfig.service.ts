a637845e2098d5258c47c257314364061d4fc3b5
hardl <philipp.pracht@sap.com>
2024-08-20 13:08:48 +0200

    switched to defined origins

--- frontend/projects/frontend-lib/src/lib/service/luigiConfig/luigiconfig.service.ts
+++ frontend/projects/frontend-lib/src/lib/service/luigiConfig/luigiconfig.service.ts
@@ -88,10 +88,10 @@ export class LuigiconfigService {
 
 
     // POC-code 
-    this.specialTenant = (window.location.origin.indexOf('localhost') >= 0 && 'cloud') || 
-      (window.location.origin.indexOf('127.0.0.1') >= 0 && 'nuage') || undefined;
-    // const isSpecialTenant = (window.location.origin.indexOf('cloudfabrik') >= 0 && 'cloud') || 
-    //   (window.location.origin.indexOf('nuage') >= 0 && 'nuage') || undefined;
+    // this.specialTenant = (window.location.origin.indexOf('localhost') >= 0 && 'cloud') || 
+    //   (window.location.origin.indexOf('127.0.0.1') >= 0 && 'nuage') || undefined;
+    const isSpecialTenant = (window.location.origin.indexOf('cloudfabrik') >= 0 && 'cloud') || 
+      (window.location.origin.indexOf('econuagetech') >= 0 && 'nuage') || undefined;
     
 
     if (this.specialTenant) {
