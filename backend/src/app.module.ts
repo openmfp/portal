beb7f78b507a06b956c76208abe86c89f72d2c2b
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-09-30 09:31:56 +0200

    feat: take content of hackaton-portal branch

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
-import { LocalServiceProviderService } from './service-providers/localServiceProviders';
+import { KubernetesServiceProviders } from './serviceProviders/kubernetesServiceProviders';
+import { OpenmfpPortalProvider } from './serviceProviders/portal-context-provider';
 import { config } from 'dotenv';
 
 config({ path: './.env' });
 
 const portalOptions: PortalModuleOptions = {
   frontendDistSources: join(__dirname, '../..', 'frontend/dist/frontend'),
-  serviceProviderService: LocalServiceProviderService,
+  serviceProviderService: KubernetesServiceProviders,
+  portalContextProvider: OpenmfpPortalProvider,
 };
 
 @Module({
