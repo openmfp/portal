5dee057047a0ea710c7fdaf114ca8fca8eeaf7fe
Grzegorz Krajniak <gkrajniak@gmail.com>
2024-10-03 16:54:26 +0200

    feat: take hackaton portal content (#39)

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { join } from 'path';
 import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
-import { LocalServiceProviderService } from './service-providers/localServiceProviders';
+import { KubernetesServiceProvidersService } from './service-providers/kubernetes-service-providers.service';
+import { OpenmfpPortalProvider } from './service-providers/portal-context-provider';
 import { config } from 'dotenv';
 
 config({ path: './.env' });
 
 const portalOptions: PortalModuleOptions = {
   frontendDistSources: join(__dirname, '../..', 'frontend/dist/frontend'),
-  serviceProviderService: LocalServiceProviderService,
+  serviceProviderService: KubernetesServiceProvidersService,
+  portalContextProvider: OpenmfpPortalProvider,
 };
 
 @Module({
--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -1,8 +1,17 @@
 import { NgModule } from '@angular/core';
-import { PortalComponent, PortalModule } from '@openmfp/portal-ui-lib';
+import {
+  PortalComponent,
+  PortalModule,
+  PortalModuleOptions,
+} from '@openmfp/portal-ui-lib';
+import { ApeiroraStaticSettingsConfigService } from './services/apeirora-static-settings-config.service';
+
+const portalOptions: PortalModuleOptions = {
+  staticSettingsConfigService: ApeiroraStaticSettingsConfigService,
+};
 
 @NgModule({
-  imports: [PortalModule.forRoot()],
+  imports: [PortalModule.forRoot(portalOptions)],
   bootstrap: [PortalComponent],
 })
 export class AppModule {}
