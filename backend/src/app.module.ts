b7f7088d834c16d4771b2fd83c8b93ee2e9f716c
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-02-13 17:41:34 +0100

    feat: update to the higer version of kubernetes client, move to es modules

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,15 +1,22 @@
 import { Module } from '@nestjs/common';
 import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
-import { join } from 'path';
-import { AccountEntityContextProvider } from './entity-context-provider/account-entity-context-provider.service';
-import { KubernetesServiceProvidersService } from './service-providers/kubernetes-service-providers.service';
-import { OpenmfpPortalProvider } from './service-providers/portal-context-provider';
+import * as path from 'node:path';
+import { AccountEntityContextProvider } from './entity-context-provider/account-entity-context-provider.service.js';
+import { KubernetesServiceProvidersService } from './service-providers/kubernetes-service-providers.service.js';
+import { OpenmfpPortalProvider } from './service-providers/portal-context-provider.js';
 import { config } from 'dotenv';
+
+const __filename = new URL(import.meta.url).pathname;
+const __dirname = path.dirname(__filename);
 
 config({ path: './.env' });
 
 const portalOptions: PortalModuleOptions = {
-  frontendDistSources: join(__dirname, '../..', 'frontend/dist/frontend/browser'),
+  frontendDistSources: path.join(
+    __dirname,
+    '../..',
+    'frontend/dist/frontend/browser'
+  ),
   entityContextProviders: {
     account: AccountEntityContextProvider,
   },
