136d5c46138684d1df6907127c5cbdc9efef6946
Grzegorz Krajniak <grzegorz.krajniak@sap.com>
2025-07-03 13:34:38 +0200

    remove not needed portal context provider

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,9 @@
-import { Module } from '@nestjs/common';
-import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
-import * as path from 'node:path';
 import { AccountEntityContextProvider } from './entity-context-provider/account-entity-context-provider.service.js';
 import { KubernetesServiceProvidersService } from './service-providers/kubernetes-service-providers.service.js';
-import { OpenmfpPortalProvider } from './service-providers/portal-context-provider.js';
+import { Module } from '@nestjs/common';
+import { PortalModule, PortalModuleOptions } from '@openmfp/portal-server-lib';
 import { config } from 'dotenv';
+import * as path from 'node:path';
 
 const __filename = new URL(import.meta.url).pathname;
 const __dirname = path.dirname(__filename);
@@ -15,14 +14,13 @@ const portalOptions: PortalModuleOptions = {
   frontendDistSources: path.join(
     __dirname,
     '../..',
-    'frontend/dist/frontend/browser'
+    'frontend/dist/frontend/browser',
   ),
   entityContextProviders: {
     account: AccountEntityContextProvider,
   },
   additionalProviders: [AccountEntityContextProvider],
   serviceProviderService: KubernetesServiceProvidersService,
-  portalContextProvider: OpenmfpPortalProvider,
 };
 
 @Module({
