e7c93e7f80ba12ca521ee9fb23d81c6f624fec6d
Sobyt483 <kravczov.andrian@mail.ru>
2025-11-04 14:34:59 +0400

    feat: address pr comment

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -1,24 +1,9 @@
 import { AppModule } from './app.module.js';
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
-import * as fs from 'fs';
 
 async function bootstrap() {
-  let app: NestExpressApplication;
-  try {
-    const key = fs.readFileSync('./openssl/key.pem');
-    const cert = fs.readFileSync('./openssl/cert.pem');
-    const httpsOptions = {
-      key,
-      cert,
-    };
-    app = await NestFactory.create<NestExpressApplication>(AppModule, {
-      httpsOptions,
-    });
-  } catch (error) {
-    app = await NestFactory.create<NestExpressApplication>(AppModule);
-  }
-
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
